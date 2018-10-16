9c6d2030ebb87925bc3f878172cd8c465c124d0b
kyu-suke <ykaneoka7@gmail.com>
2018-10-16 19:40:37 +0900

    a

---
+++ rb4.rb
@@ -0,0 +1,53 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+  
+  def initialize(name:, price:)
+    self.name = name
+    self.price = price
+  end
+  
+  def info
+    return "#{self.name} #{self.price}円"
+  end
+  
+  def get_total_price(count)
+    total_price = self.price * count
+    if count >= 3
+      total_price -= 100
+    end
+    return total_price
+  end
+end
+
+
+# require "./menu"
+
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
+
+menus = [menu1, menu2, menu3, menu4]
+
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  index += 1
+end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+
+count = gets.chomp.to_i
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
+
